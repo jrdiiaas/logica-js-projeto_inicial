342242b3f75cecbf4dcb2b747db2a90dd80bb1a1
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2024-04-24 20:16:57 -0300

    Implementado uso de crase nas strings ao invés de aspas simples para padronizar e facilitar concatenação

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-alert('Boas vindas ao jogo do número secreto!');
+alert(`Boas vindas ao jogo do número secreto!`);
 let numeroSecreto = 29;
-let chute = prompt('Faça a sua primeira tentativa, digite um número:');
+let chute = prompt(`Faça a sua primeira tentativa, digite um número:`);
 
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você acertou!');
+    console.log(`Isso aí! Você acertou!`);
 } else {
     if (chute > numeroSecreto) {
         alert(`O número secreto é menor que ${chute}, tente novamente apertando F5 no seu teclado.`);
