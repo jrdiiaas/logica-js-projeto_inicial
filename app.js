ff0e0d11100b8c8a8253461eede7e8fce459eeca
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2024-04-25 21:44:17 -0300

    Implementada a lógica de enquanto não acertar o número secreto, exibir novo prompt para continuar jogando

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 alert(`Boas vindas ao jogo do número secreto!`);
 let numeroSecreto = 29;
-let chute = prompt(`Faça a sua primeira tentativa, digite um número:`);
+let chute;
+console.log(numeroSecreto);
 
-if (chute == numeroSecreto) {
-    console.log(`Isso aí! Você acertou!`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}, tente novamente apertando F5 no seu teclado.`);
+while (chute != numeroSecreto) {
+    chute = prompt(`Faça uma jogada, digite um número:`);
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você acertou!`);
     } else {
-        alert(`O número secreto é maior que ${chute}, tente novamente apertando F5 no seu teclado.`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}, tente novamente.`);
+        } else {
+            alert(`O número secreto é maior que ${chute}, tente novamente.`);
+        }
     }
 }
