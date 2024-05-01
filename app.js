ae04adc6b677152c4b08235afb524899331e581e
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2024-05-01 19:19:46 -0300

    Comentando código do app.js para manter o histórico sem interferência no desenvolvimento

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-alert(`Boas vindas ao jogo do número secreto!`);
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
-let chute;
-let tentativa = 1;
-// console.log(numeroSecreto);
 
-while (chute != numeroSecreto) {
-    chute = prompt(`Faça uma jogada, digite um número:`);
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}, tente novamente.`);
-        } else {
-            alert(`O número secreto é maior que ${chute}, tente novamente.`);
-        }
-    }
-    tentativa++;
-}
 
-let palavraTentativa = tentativa > 1 ? `tentativas` : `tentativa`
-alert(`Isso aí! Você acertou com ${tentativa} ${palavraTentativa}.`);
+/* Primeiro Código */
+
+// alert(`Boas vindas ao jogo do número secreto!`);
+// let numeroSecreto = parseInt(Math.random() * 100 + 1);
+// let chute;
+// let tentativa = 1;
+// // console.log(numeroSecreto);
+
+// while (chute != numeroSecreto) {
+//     chute = prompt(`Faça uma jogada, digite um número:`);
+//     if (chute == numeroSecreto) {
+//         break;
+//     } else {
+//         if (chute > numeroSecreto) {
+//             alert(`O número secreto é menor que ${chute}, tente novamente.`);
+//         } else {
+//             alert(`O número secreto é maior que ${chute}, tente novamente.`);
+//         }
+//     }
+//     tentativa++;
+// }
+
+// let palavraTentativa = tentativa > 1 ? `tentativas` : `tentativa`
+// alert(`Isso aí! Você acertou com ${tentativa} ${palavraTentativa}.`);
